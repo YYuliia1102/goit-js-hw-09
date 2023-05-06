711e83df9a5536f12231fa014f3f78a20ec09d18
YYuliia1102 <reya1102@gmail.com>
2023-05-06 20:33:16 +0300

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,25 @@
+let intervalId;
 
+document.querySelector("[data-start]").addEventListener("click", startColorChange)
+document.querySelector("[data-stop]").addEventListener("click", stopColorChange)
+
+
+function startColorChange() {
+    if (!intervalId) {
+        document.querySelector("[data-start]").disabled = true;
+        intervalId = setInterval(changeBodyColor, 1000);
+    }
+}
+
+function changeBodyColor() {
+    const randomColor = `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, "0")}`;
+    console.log(randomColor);
+    document.body.style.backgroundColor = randomColor;
+}
+
+function stopColorChange() {
+    clearInterval(intervalId);
+    intervalId = null;
+    document.querySelector("[data-start]").disabled = false;
+
+}
